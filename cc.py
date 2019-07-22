ed9e2330ee21b24ace52287c95cb112923dee40b
dollarstir <kpin463@gmail.com>
2019-07-22 14:05:32 +0000

    Update cc.py
    locations

--- cc.py
+++ cc.py
@@ -1,4 +1,8 @@
 from gpiozero import Button
+
+import requests
+import json
+
 import geocoder
 g = geocoder.ip('me')
 print(g.latlng)
@@ -13,13 +17,24 @@ ambulancebutton=Button(4)
 #a function to count the number of times a button is pressed
     
 
+send_url = 'http://api.ipstack.com/156.38.115.11?access_key=7ae0ee9cacb82c78fb0780aaefafda41&format=1'
+r = requests.get(send_url)
+j = json.loads(r.text)
+lat = j['latitude']
+lon = j['longitude']
+city=j['city']
+reg=j['region_name']
+
+
 
 def police():
-        print(  "hi, I am in need of the police At",latitude,longitude )
+        print(  "hi, I am in need of the police At",latitude,longitude,city,reg )
 def fire ():
-        print(  "hi , I am in need of the fire service team at")
+        print(  "hi , I am in need of the fire service team at",latitude,longitude,city,reg )
 def ambulance():
-        print(  "hi , I am in need of the ambulance team at")
+        print(  "hi , I am in need of the ambulance team at",latitude,longitude,city,reg )
 policebutton.when_pressed =police
 firebutton.when_pressed =fire
 ambulancebutton.when_pressed =ambulance
+
+
